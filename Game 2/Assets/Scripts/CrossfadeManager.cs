10a41e5d4e0ea9aef472040c0ea396437dbb11b2
gasparo8 <gasparo8@msu.edu>
2024-08-09 17:26:30 -0400

    Added Coroutine for Crossfade Script (not complete)

--- Game 2/Assets/Scripts/CrossfadeManager.cs
+++ Game 2/Assets/Scripts/CrossfadeManager.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+/*(using UnityEngine;
 using UnityEngine.UI;
 
 public class CrossfadeManager : MonoBehaviour
@@ -8,6 +8,28 @@ public class CrossfadeManager : MonoBehaviour
 
     public void PlayCrossfadeInOut()
     {
+        crossfadeImage.enabled = true;
         crossfadeAnimator.SetTrigger("FadeInOut");
+    }
+}
+*/
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CrossfadeManager : MonoBehaviour
+{
+    public Animator crossfadeAnimator;
+    public Image crossfadeImage;
+
+    public IEnumerator PlayCrossfadeInOut()
+    {
+        crossfadeImage.enabled = true;
+        crossfadeAnimator.SetTrigger("FadeInOut");
+
+        // Wait until the FadeInOut animation is finished
+        yield return new WaitForSeconds(crossfadeAnimator.GetCurrentAnimatorStateInfo(1).length);
+        crossfadeImage.enabled = false;
     }
 }
