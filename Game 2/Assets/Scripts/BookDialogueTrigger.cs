422f6ef0f1d25cc10121e2883b343c34ef4e715b
gasparo8 <gasparo8@msu.edu>
2024-08-15 21:48:03 -0400

    Added Second Door Pound

--- Game 2/Assets/Scripts/BookDialogueTrigger.cs
+++ Game 2/Assets/Scripts/BookDialogueTrigger.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -27,5 +27,52 @@ public class BookDialogueTrigger : MonoBehaviour
     public void postReadingCutscene()
     {
         dialogueManager.StartDialogue(postReadingCutsceneDialogue);
+    }
+}
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookDialogueTrigger : MonoBehaviour
+{
+    public Dialogue bookDialogue;
+    public Dialogue bookCouchDialogue;
+    public Dialogue postReadingCutsceneDialogue;
+    public AudioSource doorPoundAudio; // Reference to the door pound audio
+    private DialogueManager dialogueManager;
+    public float doorPoundDelay = 4f;
+
+    void Start()
+    {
+        dialogueManager = FindObjectOfType<DialogueManager>();
+    }
+
+    public void GoGetBookDialogue()
+    {
+        dialogueManager.StartDialogue(bookDialogue);
+    }
+
+    public void BookToCouchDialogue()
+    {
+        dialogueManager.StartDialogue(bookCouchDialogue);
+    }
+
+    public void postReadingCutscene()
+    {
+        dialogueManager.StartDialogue(postReadingCutsceneDialogue);
+        StartCoroutine(PlayDoorPoundAfterDelay(doorPoundDelay)); // Wait for the specified delay
+    }
+
+    private IEnumerator PlayDoorPoundAfterDelay(float doorPoundDelay)
+    {
+        yield return new WaitForSeconds(doorPoundDelay);
+
+        // Play the door pound audio
+        if (doorPoundAudio != null)
+        {
+            doorPoundAudio.Play();
+        }
     }
 }
