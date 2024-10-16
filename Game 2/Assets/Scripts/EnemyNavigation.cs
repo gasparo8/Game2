db1c542fd0af282fb668d9733463206afa58871d
gasparo8 <gasparo8@msu.edu>
2024-10-15 22:00:12 -0400

    Added Walker Collider Trigger to play Stabbing Animation

--- Game 2/Assets/Scripts/EnemyNavigation.cs
+++ Game 2/Assets/Scripts/EnemyNavigation.cs
@@ -8,15 +8,38 @@ public class EnemyNavigation : MonoBehaviour
     public Transform player;
     private NavMeshAgent agent;
 
+    // Reference to the stabbing object
+    public GameObject stabbingObject;
+
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+
+        // Ensure the stabbing object is disabled at the start
+        if (stabbingObject != null)
+        {
+            stabbingObject.SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Set the walker's destination to the player's position
         agent.destination = player.position;
+    }
+
+    // Detect collision with the player
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // Check if the collided object is the player
+        {
+            // Enable the stabbing object when collision occurs
+            if (stabbingObject != null)
+            {
+                stabbingObject.SetActive(true); // This will activate the stabbing animation
+            }
+        }
     }
 }
