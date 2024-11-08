8f6745d658e5013a066eed7b9a5c8e7b73446918
gasparo8 <gasparo8@msu.edu>
2024-11-08 16:37:42 -0500

    Front Door Peeker Only Active After Pizza Cutscene Completes

--- Game 2/Assets/Scripts/TaskManagerLockDoor.cs
+++ Game 2/Assets/Scripts/TaskManagerLockDoor.cs
@@ -1,56 +1,3 @@
-/*using System.Collections.Generic;
-using TMPro;
-using UnityEngine;
-
-public class TaskManager : MonoBehaviour
-{
-    [SerializeField] private List<LockController> doors;
-    [SerializeField] private TextMeshProUGUI lockedCounterText;
-    [SerializeField] private BookDialogueTrigger bookDialogueTrigger; // Reference to the BookDialogueTrigger
-
-    private int lockedDoorsCount = 0;
-
-    private void Start()
-    {
-        lockedCounterText.gameObject.SetActive(false); // Hide the counter initially
-        UpdateLockedCounter();
-    }
-
-    public void DoorLockedStateChanged(bool isLocked)
-    {
-        if (isLocked)
-        {
-            lockedDoorsCount++;
-        }
-        else
-        {
-            lockedDoorsCount--;
-        }
-
-        UpdateLockedCounter();
-    }
-
-    private void UpdateLockedCounter()
-    {
-        if (lockedDoorsCount >= doors.Count)
-        {
-            lockedCounterText.gameObject.SetActive(false); // Hide the counter if all doors are locked
-            bookDialogueTrigger.GoGetBookDialogue(); // Trigger the book dialogue
-            this.enabled = false; // Disable this script to prevent further updates
-        }
-        else
-        {
-            lockedCounterText.text = $"{lockedDoorsCount}/{doors.Count} Doors Locked";
-        }
-    }
-
-    // Method to handle cutscene completion
-    public void OnCutsceneComplete()
-    {
-        lockedCounterText.gameObject.SetActive(true); // Show the counter
-    }
-}
-*/
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -64,10 +11,13 @@ public class TaskManager : MonoBehaviour
     private int lockedDoorsCount = 0;
     private bool allDoorsLocked = false; // Flag to track if all doors have been locked
 
+    public GameObject frontDoorPeeker;
+
     private void Start()
     {
         lockedCounterText.gameObject.SetActive(false); // Hide the counter initially
         UpdateLockedCounter();
+        frontDoorPeeker.SetActive(false);
     }
 
     public void DoorLockedStateChanged(bool isLocked)
@@ -109,6 +59,7 @@ public class TaskManager : MonoBehaviour
         if (!allDoorsLocked)
         {
             lockedCounterText.gameObject.SetActive(true); // Show the counter
+            frontDoorPeeker.gameObject.SetActive(true);
         }
     }
 }
