3741d40edfe7b616c00e5f9976f524610485d703
gasparo8 <gasparo8@msu.edu>
2024-11-29 09:14:00 -0500

    Updated Mouse Cursor for Death Animation Buttons

--- Game 2/Assets/Scripts/MainMenu.cs
+++ Game 2/Assets/Scripts/MainMenu.cs
@@ -2,16 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class MainMenu : MonoBehaviour
 {
-public void PlayGame()
+    // Starts the next scene in the build order
+    public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    // Quits the game
     public void QuitGame()
     {
         Debug.Log("You Quit!");
         Application.Quit();
+    }
+
+    // Restarts the current scene
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    // Returns to the Main Menu (assumes Main Menu is scene index 0)
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Game 2/Assets/Scripts/MouseLook.cs
+++ Game 2/Assets/Scripts/MouseLook.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -27,5 +27,87 @@ public class MouseLook : MonoBehaviour
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
 
         playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+    float xRotation = 0f;
+
+    private bool isCursorLocked = true;
+
+    void Start()
+    {
+        LockCursor();
+    }
+
+    void Update()
+    {
+        // Handle cursor locking/unlocking
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ToggleCursorLock();
+        }
+
+        /* Simulate player death for testing
+        if (Input.GetKeyDown(KeyCode.K)) // Press 'K' to simulate death
+        {
+            OnPlayerDeath();
+        }
+        */
+
+        if (isCursorLocked)
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+            playerBody.Rotate(Vector3.up * mouseX);
+        }
+    }
+
+    public void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        isCursorLocked = true;
+    }
+
+    public void UnlockCursor()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        isCursorLocked = false;
+    }
+
+    private void ToggleCursorLock()
+    {
+        if (isCursorLocked)
+        {
+            UnlockCursor();
+        }
+        else
+        {
+            LockCursor();
+        }
+    }
+
+    // Call this method when the player "dies"
+    public void OnPlayerDeath()
+    {
+        UnlockCursor();
+        Debug.Log("Player has died. Cursor unlocked.");
+        // Add any additional logic for the death screen here
     }
 }
