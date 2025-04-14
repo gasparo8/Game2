9a50a08dbc7b3681f6a85f81abf86a981e08e494
gasparo8 <gasparo8@msu.edu>
2025-04-13 21:45:23 -0400

    Second Sink Particle System Implemented

--- Game 2/Assets/Scripts/FaucetWaterEmitter.cs
+++ Game 2/Assets/Scripts/FaucetWaterEmitter.cs
@@ -2,22 +2,20 @@ using UnityEngine;
 
 public class FaucetWaterEmitter : MonoBehaviour
 {
-    [SerializeField] private ParticleSystem waterParticleSystem;
+    [SerializeField] private ParticleSystem waterParticleSystem1;
+    [SerializeField] private ParticleSystem waterParticleSystem2;
 
     public void EmitWater()
     {
-        if (waterParticleSystem != null)
-        {
-            waterParticleSystem.Play();
-            Invoke(nameof(StopWater), 5f);
-        }
+        if (waterParticleSystem1 != null) waterParticleSystem1.Play();
+        if (waterParticleSystem2 != null) waterParticleSystem2.Play();
+
+        Invoke(nameof(StopWater), 5f);
     }
 
     private void StopWater()
     {
-        if (waterParticleSystem != null)
-        {
-            waterParticleSystem.Stop();
-        }
+        if (waterParticleSystem1 != null) waterParticleSystem1.Stop();
+        if (waterParticleSystem2 != null) waterParticleSystem2.Stop();
     }
 }
