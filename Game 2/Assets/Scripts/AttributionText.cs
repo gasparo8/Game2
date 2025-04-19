535f849f76a103ba0e54d5d3a588b1ef80c0e454
gasparo8 <gasparo8@msu.edu>
2025-04-18 21:29:55 -0400

    Added Quick Footsteps in Backyard

--- Game 2/Assets/Scripts/AttributionText.cs
+++ Game 2/Assets/Scripts/AttributionText.cs
@@ -33,6 +33,7 @@ public class NewBehaviourScript : MonoBehaviour
            Adriana Lopez - Coffee Machine by Acekat13X31 -- https://freesound.org/s/515685/ -- License: Attribution 4.0
            tissue pulls.aif by trundlefly -- https://freesound.org/s/196700/ -- License: Creative Commons 0
            owl.wav by Anthousai -- https://freesound.org/s/398734/ -- License: Creative Commons 0
+           Gravel Footsteps 1 by simonjeffery13 -- https://freesound.org/s/750797/ -- License: Creative Commons 0
         */
     }
 }
