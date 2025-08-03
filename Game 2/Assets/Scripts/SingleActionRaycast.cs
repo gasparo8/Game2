1c8872a1dcd8573aa1e1ca86dde47425f02a478f
gasparo8 <gasparo8@msu.edu>
2025-08-03 10:18:04 -0400

    Added Guitar Strum Audio

--- Game 2/Assets/Scripts/SingleActionRaycast.cs
+++ Game 2/Assets/Scripts/SingleActionRaycast.cs
@@ -39,6 +39,7 @@ public class SingleActionRaycast : MonoBehaviour
     private const string fakePlant = "FakePlant"; // New tag for FakePlant
     private const string tissueBox = "TissueBox"; // Mew tag for Tissue Box
     private const string doorBell = "DoorBell";
+    private const string guitar = "Guitar";
 
     private GameObject pickedUpObject = null;
     [SerializeField] private Transform holdPoint; // Point where the object will be held
@@ -281,6 +282,28 @@ public class SingleActionRaycast : MonoBehaviour
             }
 
             else if (hit.collider.CompareTag(doorBell)) // Detect the doorBell
+            {
+                if (!doOnce)
+                {
+                    CrosshairChange(true); // Highlight the crosshair
+                }
+
+                isCrossHairActive = true;
+                doOnce = true;
+
+                if (Input.GetKeyDown(toggleLightKey)) // Use the same key as other interactions
+                {
+                    // Play the audio clip attached to the doorbell object
+                    AudioSource audioSource = hit.collider.GetComponent<AudioSource>();
+                    if (audioSource != null)
+                    {
+                        audioSource.Play(); // Play the audio clip
+                    }
+
+                }
+            }
+
+            else if (hit.collider.CompareTag(guitar)) // Detect the doorBell
             {
                 if (!doOnce)
                 {
